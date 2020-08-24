eef9feb2bb0cda422e43a113bf714b5b1be9b043
toshiki <toshiki-diary.stst@outlook.jp>
2020-08-24 06:28:53 +0000

    find_byの修正

--- test_sessions_controller.rb
+++ test_sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(session_params[:email])
+    user = User.find_by(email: session_params[:email])
     if user && user.authenticate(session_params[:password])
        log_in user
        redirect_to root_path, success: 'ログインに成功しました'
